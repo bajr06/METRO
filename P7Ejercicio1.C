de0dae8bc3fec754a5169975cd49122760f3a6ca
bajr06 <bryanandreujimenezrojas@gmail.com>
2025-01-20 10:11:58 +0100

    P7E1 Terminado

---
+++ P7Ejercicio1.C
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int Burbuja(const void * n1, const void * n2){	
+	return(*(int *)n1 - *(int *)n2);
+}
+
+void Anaidir(int * lista, const int cantidad){
+	printf("Escriba cada número a añadir a la lista\n");
+	for(int i = 0; i < cantidad; i++){
+		 scanf("%d", &lista[i]);
+	}
+}
+
+void Mostrar(int * lista, const int cantidad){
+	for(int j = 0; j < cantidad; j++){
+		printf("Número[%d]: %d\n", j, lista[j]);
+	}
+
+}
+
+
+int main(int argc, char ** argv){
+	int cantidad;
+
+	if(argc == 1){
+		printf("¿Qué cantidad de números quieres procesar?\n");
+		scanf("%d", &cantidad);
+
+		int * lista = (int *)malloc(cantidad * sizeof(int));
+
+		if(lista == NULL){
+			printf("Error, falta de memoria\n");
+			return EXIT_FAILURE;
+		}
+
+		Anaidir(&lista[0], cantidad);
+	
+		qsort(lista, cantidad, sizeof(lista[0]), Burbuja);
+
+		Mostrar(&lista[0], cantidad);
+		
+		free(lista);
+
+		return 0;
+	}
+	else
+	if(argc == 3){
+		if(strcmp(argv[1], "comparar") == 0){
+			cantidad = atoi(argv[2]);
+
+			int * lista = (int *)malloc(cantidad * sizeof(int));
+
+			if(lista == NULL){
+				printf("Error, falta de memoria\n");
+				return EXIT_FAILURE;
+			}
+
+			Anaidir(&lista[0], cantidad);
+
+			qsort(lista, cantidad, sizeof(lista[0]), Burbuja);
+
+			Mostrar(&lista[0], cantidad);
+
+			free(lista);
+
+			return 0;
+		}
+	}
+	else{
+		printf("Inválido\n");
+		return 1;
+	}
+}

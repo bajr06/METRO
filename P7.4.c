7428514e82a44531dce2a6ffa3414401b9e28e8a
Leonardo Marescutti <155202752+Lancelot-Mar@users.noreply.github.com>
2025-02-06 19:06:30 +0100

    Ejercicio 4 terminado

--- P7.4.c
+++ P7.4.c
@@ -1,23 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-#define MAX_CARACTERES 20
+#define MAX_PAL 50 
 
+bool Buscar_Letras(const char *str, int ini, int fin) {
 
+    if (ini >= fin) 
+        return true;
+    
+    if (str[ini] != str[fin]) 
+        return false;
 
-int main(){
-
-char palabra[MAX_CARACTERES];
-scanf("%s",palabra);
-
-for (int i = 0; i < MAX_CARACTERES; ++i){
-	for (int i = 0; j < MAX_CARACTERES; ++j){
-		do{
-
-		}while(strcmp(palabra+j,(strlen(palabra-1)-i)) = 1);
-	}
+    return Buscar_Letras(str, ini + 1, fin - 1);
 }
 
-return 0; 
+int Tamagno_Palabra(const char *str) {
+    
+    int len = strlen(str);
+    return Buscar_Letras(str, 0, len - 1);
+}
+
+int main(){
+    
+    char * palabra = (char *)malloc(MAX_PAL*sizeof(char));
+
+    if(palabra == NULL){ //Verifdicamos que el catalogo no sea nullo
+        printf("ERROR\n");
+        return EXIT_FAILURE; //en caso de que este sea null, devolvera un error y terminara el programa
+    }
+
+    printf("Introduce una palabra:");
+    scanf("%s", palabra);
+
+    if (Tamagno_Palabra(palabra)){
+        printf("%s es palindroma.\n", palabra);
+    }else{
+        printf("%s no es palindroma.\n", palabra);
+    }
+    
+    free(palabra);
+
+    return 0;
 }

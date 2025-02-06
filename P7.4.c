b65e4b5c065a9c9640a0674f9f03b8354b71f0e7
Lancelot-Mar <leomarescutti247@gmail.com>
2025-02-06 14:25:41 +0100

    4

---
+++ P7.4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_CARACTERES 20
+
+
+
+int main(){
+
+char palabra[MAX_CARACTERES];
+scanf("%s",palabra);
+
+for (int i = 0; i < MAX_CARACTERES; ++i){
+	for (int i = 0; j < MAX_CARACTERES; ++j){
+		do{
+
+		}while(strcmp(palabra+j,(strlen(palabra-1)-i)) = 1);
+	}
+}
+
+return 0; 
+}

5181658e02aa8e66bd84278077427a424546ed62
bajr06 <181849388+bajr06@users.noreply.github.com.>
2025-02-07 16:48:33 +0100

    Nuevos nombres de los ejercicios - Programacion

--- Ejercicio2.1.c
+++ Ejercicio2.1.c
@@ -11,6 +11,49 @@ typedef struct {
 	int nota;
 } Estudiante;
 
+void Inicializar_Estudiante(Estudiante * Inicializar, const char * Nombre, const char * Apellido, const int Nota);
+
+void Agregar_Estudiante(Estudiante * Agregar, const int cantidad);
+
+void Imprimir_Clase(Estudiante * Imprimir, int const cantidad);
+
+bool Burbuja(Estudiante * Clase, const int cantidad);
+
+bool Burbuja_Caracteres(Estudiante * Clase, const int cantidad);
+
+int main(){
+	
+	int cantidad;
+
+	puts("¿Que cantidad de estudiantes quieres procesar?");
+	scanf("%d", &cantidad);
+
+	Estudiante * Clase = (Estudiante *)malloc(cantidad * sizeof(Estudiante));
+
+	Agregar_Estudiante(Clase, cantidad);
+
+	puts("Estos son los estudiantes que hay en la clase:");
+	Imprimir_Clase(Clase,cantidad);
+
+	Burbuja(Clase, cantidad);
+	
+	printf("\n");
+
+	puts("Estos son los estudiantes ordenador por nota:");
+	Imprimir_Clase(Clase, cantidad);
+	Burbuja_Caracteres(Clase, cantidad);
+
+	puts("");
+
+	puts("Estos son los estudiantes ordenador por apellido:");
+	Imprimir_Clase(Clase, cantidad);
+
+	free(Clase);
+
+	return 0;
+}
+
+
 void Inicializar_Estudiante(Estudiante * Inicializar, const char * Nombre, const char * Apellido, const int Nota){
 	strcpy(Inicializar -> nombre, Nombre);
 	strcpy(Inicializar -> apellido, Apellido);
@@ -77,55 +120,20 @@ bool Burbuja_Caracteres(Estudiante * Clase, const int cantidad){
 	
 	do{
 		intercambios = 0;
-
-		for(int k = 0; k < cantidad - 1; k++){
-			for(int l = 0; l < MAX_CARACTERES; l++){
-				if(strncmp((*(&Clase[k] -> apellido)) + l, (*(&Clase[k+1].apellido)) + l, MAX_CARACTERES) != 0){
-					memcpy(&guardado, &Clase[k+1], sizeof(Estudiante)); // Esto no funciona guardado[0] = Clase[0];. Hay que usar memcpy. TODO: 
-					memcpy(&Clase[k], &Clase[k+1], sizeof(Estudiante));
-					memcpy(&Clase[k+1], &guardado, sizeof(Estudiante));
-					intercambios++;
-				}
-				else{
-					continue;
-				}
+		for(int k = 0; k < cantidad - 1; k++){ // Posicion del estudiante. 
+			if(strncmp(Clase[k].apellido, Clase[k+1].apellido, MAX_CARACTERES) > 0){
+				memcpy(&guardado, &Clase[k], sizeof(Estudiante));
+				memcpy(&Clase[k], &Clase[k+1], sizeof(Estudiante));
+				memcpy(&Clase[k+1], &guardado, sizeof(Estudiante));
+				intercambios++;
+			}
+			else{
+				continue;
 			}
 		}
-
+		
 	}
 	while(intercambios != 0);
 
 	return true;
-}
-
-int main(){
-	
-	int cantidad;
-
-	puts("¿Que cantidad de estudiantes quieres procesar?");
-	scanf("%d", &cantidad);
-
-	Estudiante * Clase = (Estudiante *)malloc(cantidad * sizeof(Estudiante));
-
-	Agregar_Estudiante(Clase, cantidad);
-
-/*
-	puts("Estos son los estudiantes que hay en la clase:");
-	Imprimir_Clase(Clase,cantidad);
-
-	Burbuja(Clase, cantidad);
-	
-	printf("\n");
-
-	puts("Estos son los estudiantes ordenador por nota:");
-	Imprimir_Clase(Clase, cantidad);
-*/
-	Burbuja_Caracteres(Clase, cantidad);
-
-	puts("Estos son los estudiantes ordenador por apellido:");
-	Imprimir_Clase(Clase, cantidad);
-
-	free(Clase);
-
-	return 0;
 }
---
+++ Ejercicio6.1.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define error(memoria){ \
+	if(memoria == NULL){ \
+		puts("ERROR: Falta de memoria."); \
+		exit(EXIT_FAILURE); \
+	} \
+} \
+
+
+void cifrado_cesar (char * clave, const int saltos);
+
+void descifrado_cesar (char * clave, const int saltos);
+
+
+int main(){
+	int cantidad, control;
+
+	puts("¿De cuantos carácteres será tu clave?");
+	control = scanf("%d", &cantidad);
+	
+	printf("\n");
+
+	if(control == 0){
+		printf("Recuerda meter valores enteros, no de otro tipo...\n");
+		return EXIT_FAILURE;
+	}
+	
+	char * clave = (char *)malloc(cantidad * sizeof(char));
+	error(clave);
+
+	getchar();
+	puts("Introduzca la clave:");
+	fgets(clave, cantidad, stdin);
+	clave[strlen(clave) - 1] = '\0';
+	
+	puts("");
+
+	int saltos;
+	printf("¿De cuantos saltos quieres que sea el cifrado?\n");
+	scanf("%d", &saltos);
+
+	printf("\n");
+
+	cifrado_cesar(&clave[0], saltos);
+
+	puts("");
+
+	descifrado_cesar(&clave[0], saltos);
+
+	printf("\n");
+
+	puts("Hasta aqui el cifrado César, ¡Hasta la próxima!");
+
+	free(clave);
+
+	return EXIT_SUCCESS;
+}
+
+
+void cifrado_cesar (char * clave, const int saltos){
+	printf("Cadena actual: %s\n", clave);
+
+
+	for(int i = 0; i < strlen(clave); i++){
+		printf("Número [%d]: %d\n", i, clave[i]);
+		
+		if(clave[i] >= 65 && clave[i] <= 90){
+			clave[i] += saltos;
+			
+			if(clave[i] > 90){
+				clave[i] -= 25;
+			}
+		}
+ 		else if(clave[i] >= 97 && clave[i] <= 122){
+			clave[i] += saltos;
+
+			if(clave[i] > 122){
+				clave[i] -= 25;
+			}
+		}
+		else{
+			puts("ERROR: No introduzcas cualquier otro caracter que no sea una letra");
+			exit(EXIT_FAILURE);	
+		}
+
+		printf("Número modificado [%d]: %d\n", i, clave[i]);
+	}
+
+	printf("Cadena cifrada: %s\n", clave);
+}
+
+void descifrado_cesar (char * clave, const int saltos){
+	printf("Cadena actual: %s\n", clave);
+
+
+	for(int j = 0; j < strlen(clave); j++){
+		printf("Número [%d]: %d\n", j, clave[j]);
+		
+		if(clave[j] >= 65 && clave[j] <= 90){
+			clave[j] -= saltos;
+			
+			if(clave[j] < 65){
+				clave[j] += 25;
+			}
+		}
+ 		else if(clave[j] >= 97 && clave[j] <= 122){
+			clave[j] -= saltos;
+
+			if(clave[j] < 97){
+				clave[j] += 25;
+			}
+		}
+		else{
+			puts("ERROR: No introduzcas cualquier otro caracter que no sea una letra");
+			exit(EXIT_FAILURE);	
+		}
+
+		printf("Número modificado [%d]: %d\n", j, clave[j]);
+	}
+
+	printf("Cadena descifrada: %s\n", clave);
+}

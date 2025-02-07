e582a83724025d4b71b205f9a1f457d27e665223
Leonardo Marescutti <155202752+Lancelot-Mar@users.noreply.github.com>
2025-02-07 17:12:55 +0100

    Ejercicio4.1.c comentarios

--- Ejercicio4.1.c
+++ Ejercicio4.1.c
@@ -5,22 +5,23 @@
 
 #define MAX_PAL 50 
 
+// Funcion recursiva para verificar si una palabra es un palindromo
 bool Buscar_Letras(const char *str, int ini, int fin) {
-
-    if (ini >= fin) 
+    
+    if (ini >= fin) // Caso base: si los índices se cruzan, es un palíndromo
         return true;
     
-    if (str[ini] != str[fin]) 
+    if (str[ini] != str[fin]) // Si los caracteres no coinciden, no es palíndromo
         return false;
 
-    return Buscar_Letras(str, ini + 1, fin - 1);
+    return Buscar_Letras(str, ini + 1, fin - 1); // Llamada recursiva con los siguientes caracteres
 }
 
 int main(){
     
-    char * palabra = (char *)malloc(MAX_PAL*sizeof(char));
+    char * palabra = (char *)malloc(MAX_PAL*sizeof(char)); // Resrva memoria para la palabra
 
-    if(palabra == NULL){ 
+    if(palabra == NULL){ // Verifica si la memoria fue asignada correctamente
         printf("ERROR\n");
         return EXIT_FAILURE; 
     }
@@ -28,15 +29,17 @@ int main(){
     printf("Introduce una palabra: ");
     scanf("%s", palabra);
     
-    int len = strlen(palabra);
+    int len = strlen(palabra); // Calcula la longitud de la palabra
     
+    // Llama a la función para verificar si la palabra es un palíndromo
     if (Buscar_Letras(palabra, 0, len - 1)){
         printf("%s es palindroma.\n", palabra);
-    }else{
+    }else{ //Si devuelve verdadero es palindroma si devuelve otra cosa no es palindroma
         printf("%s no es palindroma.\n", palabra);
     }
     
-    free(palabra);
+    free(palabra); // Libera la memoria asignada dinámicamente
 
     return 0;
 }
+
